ef5136493399d5a2e9b2862ff19fd75164fa0679
TonechoGomez <gptonecho@gmail.com>
2026-07-13 12:51:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,21 @@
-// Archivo de activación mínimo para cumplir los requisitos de Android
 const CACHE_NAME = 'necesidades-casa-v1';
 
+// Instalar el Service Worker
 self.addEventListener('install', (event) => {
     event.waitUntil(self.skipWaiting());
 });
 
+// Activar el Service Worker
 self.addEventListener('activate', (event) => {
     event.waitUntil(self.clients.claim());
 });
 
+// Capturar las peticiones (Obligatorio para que Chrome valide la App)
 self.addEventListener('fetch', (event) => {
-    // Deja pasar todas las peticiones a internet de forma normal
-    return;
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            // Si no hay internet, simplemente deja pasar la petición normal o falla elegantemente
+            return caches.match(event.request);
+        })
+    );
 });
